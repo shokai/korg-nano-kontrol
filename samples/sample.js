f286f48574fa1ca03daef57f663fe77cf432a117
Sho Hashimoto <hashimoto@shokai.org>
2015-06-10 15:29:41 +0900

    fixed slider and knob

--- samples/sample.js
+++ samples/sample.js
@@ -6,15 +6,19 @@ nanoKONTROL.connect().then(function(device){
   console.log(device);
 
   device.on('slider:0', function(value){
-    console.log("slider:0 => "+value);
+    console.log("slider:0 >>> "+value);
   });
 
   device.on('knob:1',function(value){
-    console.log("knob:1 => "+value);
+    console.log("knob:1 >>> "+value);
+  });
+
+  device.on('slider',function(name, value){
+    console.log("slider:"+name+" => "+value);
   });
 
   device.on('knob',function(name, value){
-    console.log("knob:"+data.name+" => "+data.value);
+    console.log("knob:"+name+" => "+value);
   });
 }).catch(function(err){
   console.error(err);
--- src/nanoKONTROL2.coffee
+++ src/nanoKONTROL2.coffee
@@ -8,14 +8,16 @@ module.exports = class nanoKONTROL2 extends EventEmitter2
     @input.on 'message', (deltaTime, message) =>
       debug message
 
-      for name in [0...7]
+      for name in [0..7]
         if message[1] is name
           @emit "slider:#{name}", message[2]
           @emit 'slider', name, message[2]
           return
 
-      for index, i of Array.prototype.splice.call([16..23], 0)
+      for name, i of Array.prototype.splice.call([16..23], 0)
         if message[1] is i
           @emit "knob:#{name}", message[2]
           @emit 'knob', name, message[2]
+          return
+
 
